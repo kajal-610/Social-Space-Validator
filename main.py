b4b63da20f576453b6cc2933a86ee7ba60291492
Kajal <65011099+kajal-610@users.noreply.github.com>
2022-12-13 13:06:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,134 +1,151 @@
+# USAGE
+# python social_distance_detector.py --input pedestrians.mp4
+# python social_distance_detector.py --input pedestrians.mp4 --output output.avi
+
+# import the necessary packages
+import config
+# from configs import config
+import detect
+import distance as dist
 import numpy as np
+import argparse
 import imutils
-import time
+import cv2 as cv
 import cv2
 import os
-import math
 
-from itertools import chain 
-from constants import *
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, default="",
+	help="path to (optional) input video file")
+ap.add_argument("-o", "--output", type=str, default="",
+	help="path to (optional) output video file")
+ap.add_argument("-d", "--display", type=int, default=1,
+	help="whether or not output frame should be displayed")
+args = vars(ap.parse_args())
 
-LABELS = open(YOLOV3_LABELS_PATH).read().strip().split('\n')
-
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype='uint8')
-
-print('Loading YOLO from disk...')
-
-neural_net = cv2.dnn.readNetFromDarknet(YOLOV3_CFG_PATH, YOLOV3_WEIGHTS_PATH)
-layer_names = neural_net.getLayerNames()
-layer_names = [layer_names[i[0] - 1] for i in neural_net.getUnconnectedOutLayers()]
-
-vs = cv2.VideoCapture(VIDEO_PATH)
-writer = None
-(W, H) = (None, None)
-
-try:
-    if(imutils.is_cv2()):
-        prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT
-    else:
-        prop = cv2.CAP_PROP_FRAME_COUNT
-    total = int(vs.get(prop))
-    print('Total frames detected are: ', total)
-except Exception as e:
-    print(e)
-    total = -1
-
+# load the COCO class labels our YOLO model was trained on
+capture=cv.VideoCapture(r"C:\Users\naran\OneDrive\Desktop\output.avi")
+print("[INFO] loading YOLO from disk...")
+print("[INFO] setting preferable backend and target to CUDA...")
 while True:
-    (grabbed, frame) = vs.read()
-
-    if not grabbed:
+    isTrue,frame=capture.read()
+    cv.imshow("output",frame)
+    if cv.waitKey(200) & 0Xff==ord("q"):
         break
-    
-    if W is None or H is None:
-        H, W = (frame.shape[0], frame.shape[1])
+capture.release()
+cv.destroyAllWindows      
+labelsPath = os.path.sep.join([config.MODEL_PATH,r"D:\project\coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+pass
 
-    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    neural_net.setInput(blob)
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([config.MODEL_PATH,r"C:\Users\DELL\Downloads\yolov3-tiny.weights"])
+configPath = os.path.sep.join([config.MODEL_PATH,r"C:\Users\DELL\Downloads\Social-distance-detection-master (2)\Social-distance-detection-master\yolo-coco\yolov3.cfg"])
 
-    start_time = time.time()
-    layer_outputs = neural_net.forward(layer_names)
-    end_time = time.time()
-    
-    boxes = []
-    confidences = []
-    classIDs = []
-    lines = []
-    box_centers = []
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
-    for output in layer_outputs:
-        for detection in output:
-            
-            scores = detection[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
-            
-            if confidence > 0.5 and classID == 0:
-                box = detection[0:4] * np.array([W, H, W, H])
-                (centerX, centerY, width, height) = box.astype('int')
-                
-                x = int(centerX - (width / 2))
-                y = int(centerY - (height / 2))
-                
-                box_centers = [centerX, centerY]
+# check if we are going to use GPU
+if config.USE_GPU:
+	# set CUDA as the preferable backend and target
+	print("[INFO] setting preferable backend and target to CUDA...")
+	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-                boxes.append([x, y, int(width), int(height)])
-                confidences.append(float(confidence))
-                classIDs.append(classID)
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-    idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.3)
-    
-    if len(idxs) > 0:
-        unsafe = []
-        count = 0
-        
-        for i in idxs.flatten():
-            
-            (x, y) = (boxes[i][0], boxes[i][1])
-            (w, h) = (boxes[i][2], boxes[i][3])
-            centeriX = boxes[i][0] + (boxes[i][2] // 2)
-            centeriY = boxes[i][1] + (boxes[i][3] // 2)
+# initialize the video stream and pointer to output video file
+print("[INFO] accessing video stream...")
+vs = cv2.VideoCapture(args[r"C:\Users\DELL\Downloads\Social-distance-detection-master (2)\Social-distance-detection-master\pedestrians.mp4"] if args["input"] else 0)
+writer = None
 
-            color = [int(c) for c in COLORS[classIDs[i]]]
-            text = '{}: {:.4f}'.format(LABELS[classIDs[i]], confidences[i])
+# loop over the frames from the video stream
+while True:
+	# read the next frame from the file
+	(grabbed, frame) = vs.read()
 
-            idxs_copy = list(idxs.flatten())
-            idxs_copy.remove(i)
+	# if the frame was not grabbed, then we have reached the end
+	# of the stream
+	if not grabbed:
+		break
 
-            for j in np.array(idxs_copy):
-                centerjX = boxes[j][0] + (boxes[j][2] // 2)
-                centerjY = boxes[j][1] + (boxes[j][3] // 2)
+	# resize the frame and then detect people (and only people) in it
+	frame = imutils.resize(frame, width=700)
+	results = detect(frame, net, ln,
+		personIdx=LABELS.index("person"))
 
-                distance = math.sqrt(math.pow(centerjX - centeriX, 2) + math.pow(centerjY - centeriY, 2))
+	# initialize the set of indexes that violate the minimum social
+	# distance
+	violate = set()
 
-                if distance <= SAFE_DISTANCE:
-                    cv2.line(frame, (boxes[i][0] + (boxes[i][2] // 2), boxes[i][1]  + (boxes[i][3] // 2)), (boxes[j][0] + (boxes[j][2] // 2), boxes[j][1] + (boxes[j][3] // 2)), (0, 0, 255), 2)
-                    unsafe.append([centerjX, centerjY])
-                    unsafe.append([centeriX, centeriY])
+	# ensure there are *at least* two people detections (required in
+	# order to compute our pairwise distance maps)
+	if len(results) >= 2:
+		# extract all centroids from the results and compute the
+		# Euclidean distances between all pairs of the centroids
+		centroids = np.array([r[2] for r in results])
+		D = dist.cdist(centroids, centroids, metric="euclidean")
 
-            if centeriX in chain(*unsafe) and centeriY in chain(*unsafe):
-                count += 1
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            else:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		# loop over the upper triangular of the distance matrix
+		for i in range(0, D.shape[0]):
+			for j in range(i + 1, D.shape[1]):
+				# check to see if the distance between any two
+				# centroid pairs is less than the configured number
+				# of pixels
+				if D[i, j] < config.MIN_DISTANCE:
+					# update our violation set with the indexes of
+					# the centroid pairs
+					violate.add(i)
+					violate.add(j)
 
-            cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-            cv2.rectangle(frame, (50, 50), (450, 90), (0, 0, 0), -1)
-            cv2.putText(frame, 'No. of people unsafe: {}'.format(count), (70, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 3)            
+	# loop over the results
+	for (i, (prob, bbox, centroid)) in enumerate(results):
+		# extract the bounding box and centroid coordinates, then
+		# initialize the color of the annotation
+		(startX, startY, endX, endY) = bbox
+		(cX, cY) = centroid
+		color = (0, 255, 0)
 
+		# if the index pair exists within the violation set, then
+		# update the color
+		if i in violate:
+			color = (0, 0, 255)
 
-    if writer is None:
+		# draw (1) a bounding box around the person and (2) the
+		# centroid coordinates of the person,
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+		cv2.circle(frame, (cX, cY), 5, color, 1)
 
-        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        writer = cv2.VideoWriter(OUTPUT_PATH, fourcc, 30,(frame.shape[1], frame.shape[0]), True)
+	# draw the total number of social distancing violations on the
+	# output frame
+	text = "Social Distancing Violations: {}".format(len(violate))
+	cv2.putText(frame, text, (10, frame.shape[0] - 25),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
 
-        if total > 0:
-            elap = (end_time - start_time)
-            print('Single frame took {:.4f} seconds'.format(elap))
-            print('Estimated total time to finish: {:.4f} seconds'.format(elap * total))
+	# check to see if the output frame should be displayed to our
+	# screen
+	if args["display"] > 0:
+		# show the output frame
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
 
-    writer.write(frame)
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
 
-print('Cleaning up...')
-writer.release()
-vs.release()                                
+	# if an output video file path has been supplied and the video
+	# writer has not been initialized, do so now
+	if args["output"] != "" and writer is None:
+		# initialize our video writer
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 25,
+			(frame.shape[1], frame.shape[0]), True)
+
+	# if the video writer is not None, write the frame to the output
+	# video file
+	if writer is not None:
+		writer.write(frame)
